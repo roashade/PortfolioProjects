8f11a25bb1ef3cec037ca471e97f08655ee7d26e
roashade <144951967+roashade@users.noreply.github.com>
2023-09-24 22:34:56 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+    <title>Add Title Here</title>
+
+    <script type="text/javascript"
+	    src="https://public.tableau.com/views/PortfolioProject_16952740711970/TorontoNeighbourhoodsCrimeReport?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"></script>
+    <script type="text/javascript"></script>
+
+</head>
+
+<body>
+
+  <h1>Add Title Here</h1>
+
+    <button id = "exportPDF">Generate PDF</button>
+    <button id = "exportImage">Generate Image</button>
+
+
+    <div id="vizContainer"></div>
+
+    <script src="./app.js"></script>
+
+</body>
+
+</html>
+
